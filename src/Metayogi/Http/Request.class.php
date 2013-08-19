349f1b6eb7ada1c489c8cfc0b8b722592c140511
Doug Macdonald <djm574@iportal-dev.usask.ca>
2013-08-18 20:50:55 -0600

    Namespace fixes

---
+++ src/Metayogi/Http/Request.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of Metayogi.
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Metayogi\Http;
+
+/**
+ * 
+ *
+ * @package Metayogi
+ * @author  Doug Macdonald <doug.macdonald@usask.ca>
+ *
+ */
+class Request extends \Symfony\Component\HttpFoundation\Request
+{
+
+}
