11fc52a105752f4ab98d92495b4a05cad798958a
Doug Macdonald <djm574@iportal-dev.usask.ca>
2013-08-22 00:57:12 -0600

    Added field class stubs

---
+++ src/Metayogi/Field/BooleanField.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of Metayogi.
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Metayogi\Field;
+
+/**
+ * Defines the Boolean Field class
+ *
+ * @package Metayogi
+ * @author  Doug Macdonald <doug.macdonald@usask.ca>
+ *
+ */
+class BooleanField extends BaseField implements FieldInterface
+{
+}
